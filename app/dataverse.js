a388d94ea118ec781e4a6233848145c3d3ce73c5
AbSattar <mdabdullsattar@gmail.com>
2023-10-30 04:10:27 +0530

    initliazed connector SDK

---
+++ app/dataverse.js
@@ -0,0 +1,6 @@
+//import Dataverse Connector SDK
+import { DataverseConnector } from '@dataverse/dataverse-connector';
+
+//initiliazez the dataverse connector class object
+const dataverseConnector = new DataverseConnector();
+
