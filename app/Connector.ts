4b03d15b60f44f0ae17573603f621e3e42862b0d
AbSattar <mdabdullsattar@gmail.com>
2023-10-31 03:23:21 +0530

    intiliazed connector

---
+++ app/Connector.ts
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+ 
+/** Import Dataverse Connector SDK and types */
+import {
+  DataverseConnector,
+  WALLET
+} from "@dataverse/dataverse-connector";
+ 
+/**
+ * Initialize the Dataverse Connector
+ */
+const dataverseConnector: DataverseConnector = new DataverseConnector();
+ 
+const App: React.FC = () => {
+  const [wallet, setWallet] = useState<WALLET>();
+ 
+  const connectWallet = async () => {
+    try {
+      const res = await dataverseConnector.connectWallet();
+      setWallet(res.wallet);
+      return(res.address);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+ 
+  return (
+    <button onClick={connectWallet}>
+      Connect Wallet
+    </button>
+  );
+};
+ 
+export default App;
